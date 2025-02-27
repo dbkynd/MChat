12aba1aacc4ed3216b7ed08b505d41869d6f8ee0
DBKynd <db@dbkynd.com>
2025-02-27 09:49:00 -0700

    add ststem_ts to status

--- apps/worker/src/server/api/services/status_service.ts
+++ apps/worker/src/server/api/services/status_service.ts
@@ -3,6 +3,7 @@ import uptime from '@repo/utilities/uptime';
 export default async function (): Promise<Status> {
   return {
     module: 'worker',
+    system_ts: Date.now(),
     uptime: uptime(),
   };
 }
--- packages/typescript-config/types/status.d.ts
+++ packages/typescript-config/types/status.d.ts
@@ -1,4 +1,5 @@
 interface Status {
   module: 'worker';
+  system_ts: number;
   uptime: string;
 }
