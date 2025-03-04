904b83c61bfad5a1e71bed0839e107f6e4b248bc
DBKynd <db@dbkynd.com>
2025-03-03 18:27:42 -0700

    axios api plugin

--- apps/worker/web/vite.config.ts
+++ apps/worker/web/vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'url';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      '^/(api|socket.io)/': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/api\//, '/api'),
+      },
+    },
+  },
+});
