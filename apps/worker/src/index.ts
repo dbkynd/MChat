076382f2fe577c00a1df5135fbb1d35b9d0b1334
DBKynd <db@dbkynd.com>
2025-03-05 15:41:41 -0700

    main application entrypoint in utility package

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -1,49 +1,5 @@
 import logger from './logger.js';
 import * as app from './app.js';
+import main from '@repo/utilities/main';
 
-async function startApp() {
-  logger.info('Starting application...');
-
-  try {
-    await app.start();
-    logger.info('Application started successfully.');
-  } catch (error) {
-    logger.error('Failed to start the application:', error);
-    process.exit(1);
-  }
-}
-
-async function stopApp() {
-  logger.info('Shutting down application...');
-
-  try {
-    await app.stop();
-    logger.info('Application shut down gracefully.');
-    process.exit(0);
-  } catch (error) {
-    logger.error('Error during shutdown:', error);
-    process.exit(1);
-  }
-}
-
-// Handle termination signals
-const shutdownSignals = ['SIGINT', 'SIGTERM'];
-shutdownSignals.forEach((signal) => {
-  process.on(signal, () => {
-    logger.info(`Received ${signal}, shutting down...`);
-    stopApp();
-  });
-});
-
-// Handle unexpected errors
-process.on('uncaughtException', (error) => {
-  logger.error('Uncaught Exception:', error);
-  process.exit(1);
-});
-
-process.on('unhandledRejection', (reason) => {
-  logger.error('Unhandled Promise Rejection:', reason);
-  process.exit(1);
-});
-
-startApp();
+main(app, logger);
