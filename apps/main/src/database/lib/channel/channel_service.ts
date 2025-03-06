0031ed9c7965b9a8bfef55e9b3fb562d47f83088
DBKynd <db@dbkynd.com>
2025-03-05 21:10:45 -0700

    add remove channels

--- apps/main/src/database/lib/channel/channel_service.ts
+++ apps/main/src/database/lib/channel/channel_service.ts
@@ -1,7 +1,17 @@
-import { Channel } from './channel_model.js';
+import { Channel, ChannelDoc } from './channel_model.js';
 
 async function list(): Promise<string[]> {
   return (await Channel.find()).map((channel) => channel.name);
 }
 
-export default { list };
+async function add(name: string): Promise<ChannelDoc> {
+  const channel = new Channel({ name });
+  await channel.save();
+  return channel;
+}
+
+async function remove(name: string): Promise<void> {
+  await Channel.findOneAndDelete({ name });
+}
+
+export default { list, add, remove };
--- apps/main/src/server/api/routes/channels.ts
+++ apps/main/src/server/api/routes/channels.ts
@@ -14,4 +14,30 @@ app.get('/', async (c) => {
   }
 });
 
+app.post('/', async (c) => {
+  const { name } = await c.req.json();
+  if (!name) return c.text('Bad Request', 400);
+
+  try {
+    const channel = await ChannelService.add(name);
+    return c.json(channel);
+  } catch (e) {
+    logger.error(e);
+    return c.text('Internal Server Error', 500);
+  }
+});
+
+app.delete('/', async (c) => {
+  const { name } = await c.req.json();
+  if (!name) return c.text('Bad Request', 400);
+
+  try {
+    await ChannelService.remove(name);
+    return c.body(null, 204);
+  } catch (e) {
+    logger.error(e);
+    return c.text('Internal Server Error', 500);
+  }
+});
+
 export default app;
