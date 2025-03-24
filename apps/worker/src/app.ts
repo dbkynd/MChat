8a3e83048fc15923e857da3b0397f68b97d34283
DBKynd <db@dbkynd.com>
2025-03-24 14:36:32 -0600

    export our Hono directly

--- apps/worker/src/app.ts
+++ apps/worker/src/app.ts
@@ -1,4 +1,4 @@
-import * as api from './server/index.js';
+import api from './server/index.js';
 import * as twitch from './twitch/chat.js';
 import { syncChannels } from './twitch/channel_manager.js';
 import { ConfigManager } from './config.js';
@@ -6,7 +6,7 @@ import { updateBaseUrl } from './axios.js';
 
 export const configManager = new ConfigManager();
 
-export async function start() {
+export async function start(): Promise<void> {
   await configManager.init();
   updateBaseUrl();
   await api.start();
@@ -14,7 +14,7 @@ export async function start() {
   await syncChannels();
 }
 
-export async function stop() {
+export async function stop(): Promise<void> {
   await twitch.disconnect();
   await api.stop();
 }
--- apps/worker/src/server/index.ts
+++ apps/worker/src/server/index.ts
@@ -2,7 +2,4 @@ import logger from '../logger.js';
 import app from './app.js';
 import Hono from '@repo/utilities/hono';
 
-const hono = Hono(app, logger);
-
-export const start = hono.start;
-export const stop = hono.stop;
+export default Hono(app, logger);
