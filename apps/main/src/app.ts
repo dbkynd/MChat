548d9c5c308381e84faa56f5b09410eb828b1f89
DBKynd <db@dbkynd.com>
2025-03-24 16:20:27 -0600

    export our Hono directly - main

--- apps/main/src/app.ts
+++ apps/main/src/app.ts
@@ -2,16 +2,16 @@ import * as pushover from '@repo/utilities/pushover';
 import * as database from './database/index.js';
 import * as elastic from './elastic/index.js';
 import logger from './logger.js';
-import * as server from './server/index.js';
+import api from './server/index.js';
 
 export async function start() {
   pushover.init(logger, 'Sync application started.');
   await database.connect();
   await elastic.init();
-  await server.start();
+  await api.start();
 }
 
 export async function stop() {
-  await server.stop();
+  await api.stop();
   await database.disconnect();
 }
--- apps/main/src/server/index.ts
+++ apps/main/src/server/index.ts
@@ -1,8 +1,5 @@
+import Hono from '@repo/utilities/hono';
 import logger from '../logger.js';
 import app from './app.js';
-import Hono from '@repo/utilities/hono';
 
-const hono = Hono(app, logger);
-
-export const start = hono.start;
-export const stop = hono.stop;
+export default Hono(app, logger);
