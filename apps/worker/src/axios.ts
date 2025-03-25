39d64d7f1e58705ba19c7e673adbdf130331e18f
DBKynd <db@dbkynd.com>
2025-03-25 15:09:20 -0600

    log the base url change

--- apps/worker/src/axios.ts
+++ apps/worker/src/axios.ts
@@ -1,12 +1,16 @@
 import axios from 'axios';
 import { configManager } from './app.js';
+import logger from './logger.js';
 
 const api = axios.create();
 
 export function updateBaseUrl(): void {
   const baseUrl = configManager.get('main_node_url');
   const apiUrl = (baseUrl || '').replace(/\/$/, '') + '/api';
-  if (baseUrl) api.defaults.baseURL = apiUrl;
+  if (baseUrl) {
+    api.defaults.baseURL = apiUrl;
+    logger.info(`Base API URL set to ${apiUrl}`);
+  }
 }
 
 export default api;
