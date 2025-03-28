be95f5cf77b92e9b651ec66b20dbdd11b14cb352
DBKynd <db@dbkynd.com>
2025-03-27 23:33:30 -0600

    add margin

--- apps/main/web/src/App.vue
+++ apps/main/web/src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="flex">
     <ChannelCRUD class="mr-4" />
-    <WorkerCRUD class="ml-4" />
-    <Calendar />
+    <WorkerCRUD class="mx-4" />
+    <Calendar class="ml-4" />
   </div>
 </template>
 
 <script setup lang="ts">
+import Calendar from '@/components/Calendar.vue';
 import ChannelCRUD from '@/components/ChannelCRUD.vue';
 import WorkerCRUD from '@/components/WorkerCRUD.vue';
-import Calendar from '@/components/Calendar.vue';
 </script>
