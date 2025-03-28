033f3041efb632df70ae86ddd9bd0f72bea73193
DBKynd <db@dbkynd.com>
2025-03-28 00:28:42 -0600

    rename Component

--- apps/main/web/src/App.vue
+++ apps/main/web/src/App.vue
@@ -2,12 +2,12 @@
   <div class="flex">
     <ChannelCRUD class="mr-4" />
     <WorkerCRUD class="mx-4" />
-    <Calendar class="ml-4" />
+    <ManualSync class="ml-4" />
   </div>
 </template>
 
 <script setup lang="ts">
-import Calendar from '@/components/Calendar.vue';
 import ChannelCRUD from '@/components/ChannelCRUD.vue';
+import ManualSync from '@/components/ManualSync.vue';
 import WorkerCRUD from '@/components/WorkerCRUD.vue';
 </script>
--- apps/main/web/src/components/ManualSync.vue
+++ apps/main/web/src/components/ManualSync.vue
@@ -3,22 +3,27 @@
     <input
       v-model="channel"
       type="text"
-      class="p-1 bg-gray-600 rounded text-white focus:outline-none w-full"
+      class="p-1 bg-gray-600 rounded text-white focus:outline-none w-full mb-2"
+      placeholder="Channel"
     />
     <input
       v-model="date"
       type="text"
-      class="p-1 bg-gray-600 rounded text-white focus:outline-none w-full"
+      class="p-1 bg-gray-600 rounded text-white focus:outline-none w-full mb-2"
+      placeholder="Date"
     />
-    <button class="bg-blue-500 hover:bg-blue-600 px-2 py-1 rounded text-sm" @click="process">
+    <button
+      class="bg-blue-500 hover:bg-blue-600 px-2 py-1 rounded text-sm text-white"
+      @click="process"
+    >
       Process
     </button>
   </div>
 </template>
 
 <script setup lang="ts">
-import api from '@/plugins/axios.js';
 import { ref } from 'vue';
+import api from '@/plugins/axios.js';
 
 const channel = ref('');
 const date = ref('');
