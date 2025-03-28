6acff6ecd077bd0703c0b8c3bece096844de2409
DBKynd <db@dbkynd.com>
2025-03-27 23:01:56 -0600

    rename a sync service method

--- apps/main/src/server/api/services/sync_service.ts
+++ apps/main/src/server/api/services/sync_service.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import WorkerService from '../../../database/lib/worker/worker_service.js';
 
-async function pushChannels() {
+async function updateWorkers() {
   const workers = await WorkerService.list();
   const urls = workers.map((url) => `${url.replace(/$\//, '')}/api/sync`);
   await Promise.all(urls.map((url) => axios.post(url).catch(() => {})));
 }
 
-export default { pushChannels };
+export default { updateWorkers };
